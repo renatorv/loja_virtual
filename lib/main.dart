b3603b944f84f8f6e3ec9594c3e29a9ebd6f51b1
renatorv <renatorv@gmail.com>
2019-08-30 10:34:00 -0300

    Configurações iniciais

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter's Clouthing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
