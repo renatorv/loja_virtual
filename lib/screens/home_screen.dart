7675a50a7f4d3b5e3fe6ef3f9ee982c8b71ee957
renatorv <renatorv@gmail.com>
2019-08-30 23:15:49 -0300

    Inicio Gradiente

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.red,
-        ),
+        HomeTab(),
       ],
     );
   }
---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    Widget _buildBodyBack() => Container(
+      
+    );
+
+    return Container();
+  }
+}
